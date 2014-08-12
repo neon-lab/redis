1adc926e08c6ae050a007e712da480a366aa4454
Sunil Mallya <mallya@neon-lab.com>
2014-08-11 17:34:10 -0700

    comment

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ license          "MIT"
 description      "Installs/Configures redis"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
-depends          'build-essential', '>= 1.4.0'
+depends          'build-essential', '>= 1.4.0' #changed from master
